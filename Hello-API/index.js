2e50001f934a02ffa8b8a1dbd064b039776ab91d
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-08-01 11:43:15 +0530

    Day-1 Hello Route

--- Hello-API/index.js
+++ Hello-API/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
 
-
+// Home 
 server.get("/", (req,resp)=>{
     resp.send("Welcome the Code Base ")
 })
 
+// Hello Route
 server.get('/hello', (req, resp)=>{
     resp.send("Hello Ravi")
 })
